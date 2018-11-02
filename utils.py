aba9f5e0e4271d138ef56da0d324b48207923302
michhar <michhar@microsoft.com>
2018-11-02 16:14:00 -0700

    install flag updates and minor code changes

--- utils.py
+++ utils.py
@@ -22,7 +22,7 @@ import shutil
 import warnings
 
 # URL from which to download the latest COCO trained weights
-COCO_MODEL_URL = "https://github.com/matterport/Mask_RCNN/releases/download/v2.0/mask_rcnn_coco.h5"
+COCO_MODEL_URL = "https://drive.google.com/file/d/1VV6WgX_RNl6a9Yi9-Pe7ZyVKHRJZSKkm/view?usp=sharing"
 
 ############################################################
 #  Bounding Boxes
